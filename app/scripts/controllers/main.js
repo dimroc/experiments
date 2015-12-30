054bc25061a4c9292817567e114814d24d6cbfa7
Dimitri Roche <dimroc@gmail.com>
2015-12-30 12:33:45 -0500

    Add row for Urban Events

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,6 +3,7 @@
 angular.module('dimrocApp')
   .controller('MainCtrl', function ($scope) {
     $scope.projects = [
+      { key: 'urban-events', title: 'Cross City Search For Media', url: 'http://urbanevents.dimroc.com/?q=graffiti' },
       { key: 'real-time-tweets', title: 'Real-Time Tweets from your Borough', url: 'http://newtweetcity.herokuapp.com/boroughs/manhattan' },
       { key: 'snapshots', title: 'Tweets as Pixels', url: 'http://newtweetcity.herokuapp.com/manhattan/snapshots/last' },
       { key: 'building_perimeters', title: 'NYC Building Perimeters in 3D', url: 'http://dimroc.github.io/nyc_building_perimeters/#/neighborhoods/lower-east-side' },
