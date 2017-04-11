43da4dbd315ceff849d970a9e3ce03b7888cc25b
Dimitri Roche <dimroc@gmail.com>
2017-04-11 11:59:35 -0400

    WIP: Entry for Machine Learning Long Ids

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,6 +3,7 @@
 angular.module('dimrocApp')
   .controller('MainCtrl', function ($scope) {
     $scope.projects = [
+      { key: 'machine-learning-long-ids', title: 'Machine Learning Long Ids', url: 'http://blog.dimroc.com/2017/02/03/video-depth-maps' },
       { key: 'left-and-depth', title: 'Left and Depth Video', url: 'http://blog.dimroc.com/2017/02/03/video-depth-maps' },
       { key: 'machine-learning-hoods', title: 'Machine Learning Hoods', url: 'http://machinelearninghoods.dimroc.com' },
       { key: 'urban-events', title: 'Cross City Search For Media', url: 'http://blog.dimroc.com/2015/12/29/search-across-cities/' },
